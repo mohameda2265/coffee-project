e853e610c52b75e72dcb99691637dfd6fa93f7b4
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2020-09-10 00:29:46 +0200

    models & SKU data added

--- coffee/models.py
+++ coffee/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CoffeeMachine(models.Model):
+    product_type = models.CharField(max_length=50)
+    water_line_compatible = models.BooleanField(default=False)
+    coffee_machine_models = models.CharField(max_length=50, default='')
+    SKU = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.SKU
+
+
+class CoffeePod(models.Model):
+    product_type = models.CharField(max_length=50)
+    coffee_flavor = models.CharField(max_length=50)
+    pack_size = models.CharField(max_length=50)
+    SKU = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.SKU
